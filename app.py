dc2ba69147ce5946de2efd882df2a50f4ce56583
Michele Molent <michele.molent.25@stud.itsaltoadriatico.it>
2026-02-24 11:48:07 +0100

    Done

--- app.py
+++ app.py
@@ -22,12 +22,45 @@ def analyze_file():
             elif c == " ":
                 spazi+=1
             count+=1
+
+    words:list = []
+
+    for r in righe:
+        words += r.split(" ")
+
+    clean_words = []
+    for w in words:
+        w = w.strip()
+        if w != "":
+            for p in [".", ",", ";", ":", "!", "?"]:
+                w = w.replace(p, "")
+            if w != "":
+                clean_words.append(w)
+
+    longest = max(clean_words, key=len)
+    shortest = min(clean_words, key=len)
+
+    frequenze = {}
+    for w in clean_words:
+        if w in frequenze:
+            frequenze[w] += 1
+        else:
+            frequenze[w] = 1
+
+    most_frequent = clean_words[0]
+    for w in frequenze:
+        if frequenze[w] > frequenze[most_frequent]:
+            most_frequent = w
     
     text_box.config(state=tk.NORMAL)
     text_box.delete(1.0, tk.END)
     text_box.insert(tk.END, "Numero totale di caratteri: " + str(count) + "\n")
     text_box.insert(tk.END, "Numero totale di segni di punteggiatura (. , ; : ! ?): " + str(punt) + "\n")
     text_box.insert(tk.END, "Numero totale di spazi: " + str(spazi) + "\n")
+    text_box.insert(tk.END, "Numero totale di parole " + str(clean_words.__len__()) + "\n")
+    text_box.insert(tk.END, "Parola più lunga " + str(longest) + "\n")
+    text_box.insert(tk.END, "Parola più corta " + str(shortest) + "\n")
+    text_box.insert(tk.END, "Parola più frequente " + str(most_frequent) + "\n")
 
 root = tk.Tk()
 root.title("Analisi file txt")
